bf2b9e0d75b02ea312420634e686530ae15fcfc5
ANSHIKA KUMARI <101940406+Anshika2022@users.noreply.github.com>
2024-12-26 22:06:56 +0530

    Two Sum - Pair with Given Sum.cpp

---
+++ Two Sum - Pair with Given Sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) {
+        // code here
+        unordered_map<int,int> mm;
+        for(int x:arr){
+            if(mm[target-x])return true;
+            mm[x]=1;
+        }
+        return false;
+    }
+};
